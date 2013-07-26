c28ef748101232836683f48c08dba017c667f8ff
andresfgm <andres.garcia@greenfieldcrm.com>
2013-07-25 20:33:41 -0500

    4

---
+++ custom/modules/Opportunities/views/view.buscarinstitucion.php
@@ -0,0 +1,31 @@
+<?php
+require_once('include/MVC/View/views/view.edit.php');
+require_once("include/JSON.php");
+
+
+class OpportunitiesViewBuscarinstitucion extends ViewEdit {
+
+ 	function OpportunitiesViewBuscarinstitucion(){
+ 		parent::ViewEdit();
+ 	}
+
+ 	function process() {
+		$this->display();
+ 	}
+
+ 	function display(){
+            echo '
+                <table>
+                <tr>
+                    <td>Filtro:</td>
+                    <td><input type="text" name="filtro" id="filtro" /></td>
+                </tr>
+                </table>
+                <input type="button" name="buscar" id="buscar" value="Buscar" onclick="buscarInstitucion();" />
+                ';
+            echo '<div id="resultado_institucion_div" ></div>';
+
+
+ 	}
+}
+?>
