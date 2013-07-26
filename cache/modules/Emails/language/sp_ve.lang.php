5f50056112821c6da81ef586b7d668dc94f2f4f9
Jose Sambrano Montesdeoca <jose.sambrano@greenfieldcrm.com>
2013-07-25 21:39:37 -0500

    Nuevamente se estan subiendo las parametrizacion de catalogos

--- cache/modules/Emails/language/sp_ve.lang.php
+++ cache/modules/Emails/language/sp_ve.lang.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-// created: 2013-07-25 18:18:38
-=======
-// created: 2013-07-26 01:23:43
->>>>>>> f9efc85935ae112b2273d8db41fca60c7e03af4e
+// created: 2013-07-25 22:38:44
 $mod_strings = array (
   'LBL_FW' => 'RV:',
   'LBL_RE' => 'RE:',
