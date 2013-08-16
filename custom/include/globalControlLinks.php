af559a81b5617371143a5ccbf625871283190a4f
Jose Sambrano Montesdeoca <jose.sambrano@greenfieldcrm.com>
2013-08-16 06:22:34 -0500

    Se corrigio una inclusion , en linux hacia que caiga la conexion a la base de datos , en el archivo custum/include/globalControlLinks

--- custom/include/globalControlLinks.php
+++ custom/include/globalControlLinks.php
@@ -40,54 +40,48 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * Description:  controls which link show up in the upper right hand corner of the app
  ********************************************************************************/
 
-//global $app_strings, $current_user;
-//global $sugar_config, $sugar_version, $sugar_flavor, $server_unique_key, $current_language, $action;
-//
-//
-//
-// if(!isset($global_control_links)){
-// 	$global_control_links = array();
-//	$sub_menu = array();
-// }
-//
-//$global_control_links=array();
-//$global_control_links['alertas'] = array(
-//'linkinfo' => array($app_strings['LBL_ALERTAS']=> 'index.php?module=Accounts&action=alertas'),
-//'submenu' => ''
-//);
-//
-//$global_control_links['employees'] = array(
-//'linkinfo' => array($app_strings['LBL_EMPLOYEES']=> 'index.php?module=Employees&action=index&query=true'),
-//'submenu' => ''
-//);
-//if (
-//        is_admin($current_user)
-//
-//        ) $global_control_links['admin'] = array(
-//
-//'linkinfo' => array($app_strings['LBL_ADMIN'] => 'index.php?module=Administration&action=index'),
-//'submenu' => ''
-//);
-//$global_control_links['training'] = array(
-//'linkinfo' => array($app_strings['LBL_TRAINING'] => 'javascript:void(window.open(\'http://support.sugarcrm.com\'))'),
-//'submenu' => ''
-// );
-//
-///* no longer goes in the menubar - now implemented in the bottom bar.
-//$global_control_links['help'] = array(
-//    'linkinfo' => array($app_strings['LNK_HELP'] => ' javascript:void window.open(\'index.php?module=Administration&action=SupportPortal&view=documentation&version='.$sugar_version.'&edition='.$sugar_flavor.'&lang='.$current_language.'&help_module='.$GLOBALS['module'].'&help_action='.$action.'&key='.$server_unique_key.'\')'),
-//    'submenu' => ''
-// );
-//*/
-//
-//$global_control_links['users'] = array(
-//'linkinfo' => array($app_strings['LBL_LOGOUT'] => 'index.php?module=Users&action=Logout'),
-//'submenu' => ''
-//);
-//
-//$global_control_links['about'] = array('linkinfo' => array($app_strings['LNK_ABOUT'] => 'index.php?module=Home&action=About'),
-//'submenu' => ''
-//);
-//
+global $app_strings, $current_user;
+global $sugar_config, $sugar_version, $sugar_flavor, $server_unique_key, $current_language, $action;
+
+
+
+ if(!isset($global_control_links)){
+ 	$global_control_links = array();
+	$sub_menu = array();
+ }
+
+$global_control_links=array();
+$global_control_links['alertas'] = array(
+'linkinfo' => array($app_strings['LBL_ALERTAS']=> 'index.php?module=Accounts&action=alertas'),
+'submenu' => ''
+);
+
+$global_control_links['employees'] = array(
+'linkinfo' => array($app_strings['LBL_EMPLOYEES']=> 'index.php?module=Employees&action=index&query=true'),
+'submenu' => ''
+);
+if (
+        is_admin($current_user)
+
+        ) $global_control_links['admin'] = array(
+
+'linkinfo' => array($app_strings['LBL_ADMIN'] => 'index.php?module=Administration&action=index'),
+'submenu' => ''
+);
+$global_control_links['training'] = array(
+'linkinfo' => array($app_strings['LBL_TRAINING'] => 'javascript:void(window.open(\'http://support.sugarcrm.com\'))'),
+'submenu' => ''
+ );
+
+
+$global_control_links['users'] = array(
+'linkinfo' => array($app_strings['LBL_LOGOUT'] => 'index.php?module=Users&action=Logout'),
+'submenu' => ''
+);
+
+$global_control_links['about'] = array('linkinfo' => array($app_strings['LNK_ABOUT'] => 'index.php?module=Home&action=About'),
+'submenu' => ''
+);
+
 
 ?>
