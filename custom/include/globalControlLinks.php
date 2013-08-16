b1216b5625a9584e8b2b8dcf7ad72514310d2830
Jose Sambrano Montesdeoca <jose.sambrano@greenfieldcrm.com>
2013-08-16 06:18:22 -0500

    Se comento las lineas para el menu superior , probablemente la causa de caida del server

--- custom/include/globalControlLinks.php
+++ custom/include/globalControlLinks.php
@@ -15,7 +15,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * 
+- * 
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -40,55 +40,54 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * Description:  controls which link show up in the upper right hand corner of the app
  ********************************************************************************/
 
-global $app_strings, $current_user;
-global $sugar_config, $sugar_version, $sugar_flavor, $server_unique_key, $current_language, $action;
+//global $app_strings, $current_user;
+//global $sugar_config, $sugar_version, $sugar_flavor, $server_unique_key, $current_language, $action;
+//
+//
+//
+// if(!isset($global_control_links)){
+// 	$global_control_links = array();
+//	$sub_menu = array();
+// }
+//
+//$global_control_links=array();
+//$global_control_links['alertas'] = array(
+//'linkinfo' => array($app_strings['LBL_ALERTAS']=> 'index.php?module=Accounts&action=alertas'),
+//'submenu' => ''
+//);
+//
+//$global_control_links['employees'] = array(
+//'linkinfo' => array($app_strings['LBL_EMPLOYEES']=> 'index.php?module=Employees&action=index&query=true'),
+//'submenu' => ''
+//);
+//if (
+//        is_admin($current_user)
+//
+//        ) $global_control_links['admin'] = array(
+//
+//'linkinfo' => array($app_strings['LBL_ADMIN'] => 'index.php?module=Administration&action=index'),
+//'submenu' => ''
+//);
+//$global_control_links['training'] = array(
+//'linkinfo' => array($app_strings['LBL_TRAINING'] => 'javascript:void(window.open(\'http://support.sugarcrm.com\'))'),
+//'submenu' => ''
+// );
+//
+///* no longer goes in the menubar - now implemented in the bottom bar.
+//$global_control_links['help'] = array(
+//    'linkinfo' => array($app_strings['LNK_HELP'] => ' javascript:void window.open(\'index.php?module=Administration&action=SupportPortal&view=documentation&version='.$sugar_version.'&edition='.$sugar_flavor.'&lang='.$current_language.'&help_module='.$GLOBALS['module'].'&help_action='.$action.'&key='.$server_unique_key.'\')'),
+//    'submenu' => ''
+// );
+//*/
+//
+//$global_control_links['users'] = array(
+//'linkinfo' => array($app_strings['LBL_LOGOUT'] => 'index.php?module=Users&action=Logout'),
+//'submenu' => ''
+//);
+//
+//$global_control_links['about'] = array('linkinfo' => array($app_strings['LNK_ABOUT'] => 'index.php?module=Home&action=About'),
+//'submenu' => ''
+//);
+//
 
- if(!isset($global_control_links)){
- 	$global_control_links = array();
-	$sub_menu = array();
- }
-if(isset( $sugar_config['disc_client']) && $sugar_config['disc_client']){
-	require_once('modules/Sync/headermenu.php');
-}
-
-
-$global_control_links['employees'] = array(
-'linkinfo' => array($app_strings['LBL_EMPLOYEES']=> 'index.php?module=Employees&action=index&query=true'),
-'submenu' => ''
-);
-if (
-        is_admin($current_user)
-
-        ) $global_control_links['admin'] = array(
-
-'linkinfo' => array($app_strings['LBL_ADMIN'] => 'index.php?module=Administration&action=index'),
-'submenu' => ''
-);
-$global_control_links['training'] = array(
-'linkinfo' => array($app_strings['LBL_TRAINING'] => 'javascript:void(window.open(\'http://support.sugarcrm.com\'))'),
-'submenu' => ''
- );
-
-/* no longer goes in the menubar - now implemented in the bottom bar.
-$global_control_links['help'] = array(
-    'linkinfo' => array($app_strings['LNK_HELP'] => ' javascript:void window.open(\'index.php?module=Administration&action=SupportPortal&view=documentation&version='.$sugar_version.'&edition='.$sugar_flavor.'&lang='.$current_language.'&help_module='.$GLOBALS['module'].'&help_action='.$action.'&key='.$server_unique_key.'\')'),
-    'submenu' => ''
- );
-*/
-
-$global_control_links['users'] = array(
-'linkinfo' => array($app_strings['LBL_LOGOUT'] => 'index.php?module=Users&action=Logout'),
-'submenu' => ''
-);
-
-$global_control_links['about'] = array('linkinfo' => array($app_strings['LNK_ABOUT'] => 'index.php?module=Home&action=About'),
-'submenu' => ''
-);
-
-if (sugar_is_file('custom/include/globalControlLinks.php')) {
-    include('custom/include/globalControlLinks.php');
-}
-if (sugar_is_file('custom/application/Ext/GlobalLinks/links.ext.php')) {
-    include('custom/application/Ext/GlobalLinks/links.ext.php');
-}
 ?>
